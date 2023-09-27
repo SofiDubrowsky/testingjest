4457041f62e9afec82a95f6a6b8bcab552c8b506
Sofia Dubrowsky <sofidubrowsky@hotmail.com.ar>
2023-09-27 16:47:19 -0300

    UserForm.js App.js

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,20 +1,28 @@
 import { useState } from "react"
 
-function UserForm() {
+function UserForm({ onUserAdd }) {
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        onUserAdd({ name, email });
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>Name</label>
-                <input/>
+                <input value={name} onChange={(e) => setName(e.target.value)} />
             </div>
             <div>
                 <label>Email</label>
-                <input/>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
             <button>Add User</button>
         </form>
     )
 }
+
+export default UserForm;
